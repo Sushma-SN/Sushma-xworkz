b8b1bb62788645b13d4b11750c7683a7de8b1b83
Sushma-SN <72926817+Sushma-SN@users.noreply.github.com>
2020-10-16 19:22:25 +0530

    First commit

---
+++ Namaskara.java
@@ -0,0 +1,21 @@
+class Wish
+{
+//this is comments
+//To start a program we need a method(main)
+//main method is an entry point
+public static void main(String[] sushma)
+{
+System.out.println("Namaskara");
+}
+
+}
+class Camera{public static void main(String camera[]){
+System.out.println("Camera is Kodak");
+System.out.println("price is lot");
+}}
+class DisplayName{
+public static void main(String[] name)
+{
+System.out.println("X-workz");
+}
+}
