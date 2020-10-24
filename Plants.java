7b5fc7638b7b28c422f1a2d0897015df6d9a30da
Sushma-SN <72926817+Sushma-SN@users.noreply.github.com>
2020-10-24 12:50:28 +0530

    Update Plants.java

--- Plants.java
+++ Plants.java
@@ -1,8 +1,8 @@
 public class Plants{
-	public static void leaves()
+	public static void leaves(String wild)
 	{
 		System.out.println("invoked leaves from Plants class");
-	
+		System.out.println("Element @ index 0 "+wild);
 	
 	}
 
