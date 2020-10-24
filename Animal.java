2601b840afcf78eaac79defa47c10234ecb9bcb9
Sushma-SN <72926817+Sushma-SN@users.noreply.github.com>
2020-10-24 12:46:47 +0530

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,7 +2,7 @@ public class Animal{
 	public static void main(String[] wild)
 	{
 		System.out.println("JVM invoked main method from Animal");
-		Plants.leaves();
+		Plants.leaves(wild[0]);
 		System.out.println("EXIT : main method from Animal");
 	
 	}
