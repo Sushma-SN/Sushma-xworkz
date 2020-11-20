6eafff6a2774bfd7983b8f6f5e8149958911c6ed
Sushma-SN <72926817+Sushma-SN@users.noreply.github.com>
2020-11-20 21:10:53 +0530

    import(nov19)

---
+++ anniversary/festival/Event.java
@@ -0,0 +1,9 @@
+package com.xworkz.anniversary.festival;
+
+public class Event {
+	public static void play() {
+		System.out.println("Invoked play");
+		System.out.println("Rules for playing games");
+		System.out.println("EXIT :: play");
+	}
+}
