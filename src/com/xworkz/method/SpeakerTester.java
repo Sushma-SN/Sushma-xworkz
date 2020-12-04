1b39fc8bc0482ae09bf6a46e046c4ac4b5a312b4
Sushma-SN <72926817+Sushma-SN@users.noreply.github.com>
2020-12-05 00:36:08 +0530

    instance-method(dec3)

---
+++ src/com/xworkz/method/SpeakerTester.java
@@ -0,0 +1,29 @@
+package com.xworkz.method;
+	import com.xworkz.method.electronic.Speaker;
+
+	public class SpeakerTester {
+
+		public static void main(String[] args) {
+			Speaker speaker = new Speaker("RED", 6);
+			System.out.println(speaker.getCompanyName());
+
+			System.out.println(speaker.isConnected());
+
+			speaker.increaseVolume();
+
+			speaker.onOrOff();
+
+			speaker.increaseVolume();
+			speaker.increaseVolume();
+			speaker.increaseVolume();
+			speaker.increaseVolume();
+			speaker.increaseVolume();
+			speaker.decreaseVolume();
+			speaker.decreaseVolume();
+			speaker.decreaseVolume();
+			speaker.decreaseVolume();
+			speaker.decreaseVolume();
+			speaker.onOrOff();
+		}
+
+	}
